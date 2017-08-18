0d442f6ad1bbc7d67b87acb1401f2a8f6e8a005f
Michal 'vorner' Vaner <vorner@vorner.cz>
2017-08-18 08:18:33 +0200

    Implement some error handling

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
+#[macro_use]
+extern crate error_chain;
 extern crate regex;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
 
-use std::io::{self, BufRead, Read, Result, Write};
+use std::io::{self, BufRead, Read, Write};
+use std::num::ParseIntError;
 
 use regex::Regex;
 use serde_json::Value;
+
+error_chain! {
+    foreign_links {
+        Io(io::Error);
+        SerdeJson(serde_json::Error);
+        NumParse(ParseIntError);
+    }
+    errors {
+        HeaderMissing {
+            description("Important header missing")
+            display("Important header missing")
+        }
+    }
+}
 
 #[derive(Serialize, Deserialize)]
 struct Call {
@@ -28,18 +45,23 @@ fn run() -> Result<()> {
         while input.read_line(&mut headers)? > 2 { }
         eprintln!("{:?}", headers);
         // Less of unwraps
-        let size = re.captures_iter(&headers).next().unwrap()[1].parse::<usize>().unwrap();
+        let size = re.captures_iter(&headers)
+            .next()
+            .ok_or(ErrorKind::HeaderMissing)?[1]
+            .parse::<usize>()?;
         let mut data = vec![0u8; size];
         input.read_exact(&mut data)?;
-        // Less of unwraps
-        let value = serde_json::from_slice::<Call>(&data).unwrap();
-        let data = serde_json::to_vec(&value).unwrap();
-        let output = io::stdout();
-        let mut output = output.lock();
-        write!(output, "Content-Length: {}\r\n\r\n", data.len())?;
-        output.write_all(&data)?;
-        output.flush()?;
-        eprintln!("Flush");
+        if let Ok(value) = serde_json::from_slice::<Call>(&data) {
+            let data = serde_json::to_vec(&value)?;
+            let output = io::stdout();
+            let mut output = output.lock();
+            write!(output, "Content-Length: {}\r\n\r\n", data.len())?;
+            output.write_all(&data)?;
+            output.flush()?;
+            eprintln!("Flush");
+        } else {
+            eprintln!("Format error, ignoring");
+        }
     }
 }
 
